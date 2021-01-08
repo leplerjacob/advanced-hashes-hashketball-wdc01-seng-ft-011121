64366711b8073ed06bf9e40a7538b3fdf4853a93
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-08 22:44:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,7 +149,7 @@ end
 def num_points_scored(name)
   game_hash[:away][:players].each do |player|
     if player[:player_name] == name
-      binding.pry
+      # binding.pry
       return player[:points]
     end
   end
